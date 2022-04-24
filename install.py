928ef93ecda642da4390a66ebe386f562f9e27ad
zhengyo <95407088+zhengyo@users.noreply.github.com>
2022-04-24 20:37:59 +0800

    Update install.py

--- install.py
+++ install.py
@@ -2,7 +2,8 @@ import os
 install1 = input("Please return to your country, such as'China' : ")
 if install1.title() == 'china':
     os.system('bash core/change_sources.sh')
-    os.system('echo Install the upgrade. && pkg up')
+    os.system('echo Install the upgrade. && apt update && apt upgrade -y')
+    os.system('bash core/change_sources.sh')
     os.system('cd core/ && python start.py')
     print("Done.")
 else:
