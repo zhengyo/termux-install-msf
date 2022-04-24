7318f12a038f47d3ce0aa6b76d748d06ad5350da
zhengyo <95407088+zhengyo@users.noreply.github.com>
2022-04-24 20:35:26 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,7 +1,7 @@
 import os
 install1 = input("Please return to your country, such as'China' : ")
 if install1.title() == 'china':
-    os.system('bash core/change-sources.sh')
+    os.system('bash core/change_sources.sh')
     os.system('echo Install the upgrade. && pkg up')
     os.system('cd core/ && python start.py')
     print("Done.")
