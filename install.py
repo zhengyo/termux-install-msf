a3a4eb610902b5a8eb8fd39a7fecc602ef6fa72b
zhengyo <95407088+zhengyo@users.noreply.github.com>
2022-04-24 20:30:33 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,7 +1,6 @@
 import os
 install1 = input("Please return to your country, such as'China' : ")
 if install1.title() == 'china':
-    os.system('sed -i 's@^(deb.*stable main)$@#\1\ndeb https://mirrors.hit.edu.cn/termux stable main@' $PREFIX/etc/apt/sources.list ')
     os.system('echo Install the upgrade. && pkg up')
     os.system('cd core/ && python start.py')
     print("Done.")
