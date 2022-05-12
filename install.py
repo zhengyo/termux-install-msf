8bb6e1876bab7de1f89cb065c261d2d6ef89c60e
zhengyo <95407088+zhengyo@users.noreply.github.com>
2022-05-12 18:40:47 +0800

    Update install.py

--- install.py
+++ install.py
@@ -4,6 +4,7 @@ install = instal.title()
 if install == 'China':
     os.system('bash core/change_sources.sh')
     os.system('echo Install the upgrade. && apt update && apt upgrade -y')
+    print("安装准备工作完成")
     os.system('bash core/change_sources.sh')
     os.system('cd core/ && python start.py')
     print("Done.")
