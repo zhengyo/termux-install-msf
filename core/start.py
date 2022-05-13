d2c9d843d8868b851838b4fa43c059a0d99ed2f2
zhengyo <95407088+zhengyo@users.noreply.github.com>
2022-05-13 19:32:07 +0800

    Update start.py

--- core/start.py
+++ core/start.py
@@ -1,7 +1,8 @@
 import os
 os.system("rm -rf $HOME/metasploit-framework/")
 os.system("apt remove ruby && rm -rf $PREFIX/lib/ruby")
-os.system("pkg install ruby python libiconv zlib autoconf bison clang coreutils curl findutils git apr apr-util libffi libgmp libpcap postgresql readline libsqlite openssl libtool libxml2 libxslt ncurses pkg-config wget make libgrpc termux-tools ncurses-utils ncurses unzip zip tar termux-elf-cleaner -y")
+os.system("pkg install ruby -y")
+os.system("pkg install python libiconv zlib autoconf bison clang coreutils curl findutils git apr apr-util libffi libgmp libpcap postgresql readline libsqlite openssl libtool libxml2 libxslt ncurses pkg-config wget make libgrpc termux-tools ncurses-utils ncurses unzip zip tar termux-elf-cleaner -y")
 print("[-] In order to ensure the success of the download, we will execute the download command 4 times, if there are 3 errors, please ignore. ")
 os.system("cd $HOME && git clone https://github.com/rapid7/metasploit-framework.git --depth=1")
 os.system("cd $HOME && git clone https://github.com/rapid7/metasploit-framework.git --depth=1")
