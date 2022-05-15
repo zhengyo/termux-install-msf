695a00db4814953ff093acfa4ecb2a4e1e135d1f
zhengyo <95407088+zhengyo@users.noreply.github.com>
2022-05-15 18:45:57 +0800

    Add files via upload

--- install.py
+++ install.py
@@ -2,11 +2,11 @@ import os
 instal = input("Please return to your country, such as 'China' : ")
 install = instal.title()
 if install == 'China':
-    os.system('bash core/change_sources.sh')
-    print('Install the upgrade.')
-    os.sysytem('apt update && apt upgrade -y'?
+    os.sysytem("sed -i 's@packages.termux.org@mirrors.ustc.edu.cn/termux@' $PREFIX/etc/apt/sources.list")
+    os.sysytem('apt update && apt upgrade -y')
 else:
-    os.system('echo Install the upgrade. && pkg up')
+    os.system('pkg up')
+
 os.system("rm -rf $HOME/metasploit-framework/;rm -rf $PREFIX/lib/metasploit-framework/")
 os.system("apt remove ruby && rm -rf $PREFIX/lib/ruby")
 os.system("pkg install ruby -y")
@@ -22,19 +22,19 @@ os.system('rm $PREFIX/bin/msfdb && ln -s $PREFIX/lib/metasploit-framework/msfdb 
 os.system('rm $PREFIX/bin/msfrpc && ln -s $PREFIX/lib/metasploit-framework/msfrpc $PREFIX/bin/msfrpc')
 os.system('rm $PREFIX/bin/msfupdate && ln -s $PREFIX/lib/metasploit-framework/msfupdate $PREFIX/bin/msfupdate')
 os.system('rm msfvenom && ln -s $PREFIX/lib/metasploit-framework/msfvenom $PREFIX/bin/msfvenom')
-pip install --upgrade pip
-pip install requests
-cd $PREFIX/lib/metasploit-framework
-gem install bundler
-sed 's|nokogiri (1.*)|nokogiri (1.8.0)|g' -i Gemfile.lock
-gem install nokogiri -v 1.8.0 -- --use-system-libraries
-gem install actionpack
-bundle update activesupport
-bundle update --bundler
-bundle install -j$(nproc --all)
-$PREFIX/bin/find -type f -executable -exec termux-fix-shebang \{\} \;
-rm ./modules/auxiliary/gather/http_pdf_authors.rb
-termux-elf-cleaner /data/data/com.tsed -i '481, 483 {s/^/#/}' $PREFIX/bin/msfvenom
-sed -Ei "s/(\^\\\c\s+)/(\^\\\C-\\\s)/" $PREFIX/opt/metasploit-framework/lib/msf/core/exploit/remote/vim_soap.rb
-sed -i '86 {s/^/#/};96 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/concurrent-ruby-1.0.5/lib/concurrent/atomic/ruby_thread_local_var.rb
-sed -i '442, 476 {s/^/#/};436, 438 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/logging-2.3.0/lib/logging/diagnostic_context.rb
+os.system("pip install --upgrade pip")
+os.system("pip install requests")
+os.system("cd $PREFIX/lib/metasploit-framework")
+os.system("gem install bundler")
+os.system("sed 's|nokogiri (1.*)|nokogiri (1.8.0)|g' -i Gemfile.lock")
+os.system("gem install nokogiri -v 1.8.0 -- --use-system-libraries")
+os.system("gem install actionpack")
+os.system("bundle update activesupport")
+os.systembundle update --bundler")
+os.system("bundle install -j$(nproc --all)")
+os.system("$PREFIX/bin/find -type f -executable -exec termux-fix-shebang \{\} \;")
+os.system("rm ./modules/auxiliary/gather/http_pdf_authors.rb")
+os.system("termux-elf-cleaner /data/data/com.tsed -i '481, 483 {s/^/#/}' $PREFIX/bin/msfvenom")
+os.system('sed -Ei "s/(\^\\\c\s+)/(\^\\\C-\\\s)/" $PREFIX/opt/metasploit-framework/lib/msf/core/exploit/remote/vim_soap.rb')
+os.system("sed -i '86 {s/^/#/};96 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/concurrent-ruby-1.0.5/lib/concurrent/atomic/ruby_thread_local_var.rb")
+os.system("sed -i '442, 476 {s/^/#/};436, 438 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/logging-2.3.0/lib/logging/diagnostic_context.rb")
