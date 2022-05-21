04a38fb491492b3da0fd5210ddd697cf06634f00
zhengyo <95407088+zhengyo@users.noreply.github.com>
2022-05-21 08:55:05 +0800

    Update install_msf.py

--- install_msf.py
+++ install_msf.py
@@ -4,15 +4,10 @@ os.system("rm -rf $HOME/metasploit-framework/;rm -rf $PREFIX/lib/metasploit-fram
 os.system("apt remove ruby && rm -rf $PREFIX/lib/ruby")
 os.system("pkg install ruby -y")
 os.system("pkg install python libiconv zlib autoconf bison clang coreutils curl findutils git apr apr-util libffi libgmp libpcap postgresql readline libsqlite openssl libtool libxml2 libxslt ncurses pkg-config wget make libgrpc termux-tools ncurses-utils ncurses unzip zip tar termux-elf-cleaner -y")
-install.title() = input("Current country: ")
-if install == 'China':
-    print("[-] In order to ensure the success of the download, we will execute the download command 4 times, if there are 3 errors, please ignore. ")
-    os.system("cd $PREFIX/lib && git clone https://github.com/rapid7/metasploit-framework.git --depth=1")
-    os.system("cd $PREFIX/lib && git clone https://github.com/rapid7/metasploit-framework.git --depth=1")
-    os.system("cd $PREFIX/lib && git clone https://github.com/rapid7/metasploit-framework.git --depth=1")
-else:
-    os.system("cd $PREFIX/lib && git clone https://github.com/rapid7/metasploit-framework.git --depth=1")
-
+print("[-] In order to ensure the success of the download, we will execute the download command 4 times, if there are 3 errors, please ignore. ")
+os.system("cd $PREFIX/lib && git clone https://github.com/rapid7/metasploit-framework.git --depth=1")
+os.system("cd $PREFIX/lib && git clone https://github.com/rapid7/metasploit-framework.git --depth=1")
+os.system("cd $PREFIX/lib && git clone https://github.com/rapid7/metasploit-framework.git --depth=1")
 os.system("cd $PREFIX/lib && git clone https://github.com/rapid7/metasploit-framework.git --depth=1")
 os.system('rm $PREFIX/bin/msfconsole && ln -s $PREFIX/lib/metasploit-framework/msfconsole $PREFIX/bin/msfconsole')
 os.system('rm $PREFIX/bin/msfd && ln -s $PREFIX/lib/metasploit-framework/msfd $PREFIX/bin/msfd')
